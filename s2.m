4a1a42af9b3eb3feb1c548b79ca90cc6461f2b95
K-A-Dastgerdi <k.ahmadidastgerdi@brighton.ac.uk>
2025-03-18 13:37:36 +0000

    Update s2.m

--- s2.m
+++ s2.m
@@ -1,11 +1,3 @@
 function time=s2(Ts,Tf)
-<<<<<<< HEAD
-<<<<<<< HEAD
 time=-pi:Ts:Tf;
-=======
-time=0:Ts:Tf;
->>>>>>> b34f707 (next change)
-=======
-time=-pi:Ts:Tf;
->>>>>>> 2212a58 (-pi added)
 end
